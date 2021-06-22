4770a1e5d54f457da2394f847fa8347cb7656c96
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-06-22 16:16:51 +0200

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+const app = new Vue(
+    {
+      el: '#app',
+      
+      data:{
+        counter : 0,
+        src : [''],
+
+      },
+  
+      methods:{
+        nomeFunzione(){
+          this.variabile;
+        },
+        nomeFunzione2(){
+          this.variabile;
+        }
+      }
+    }
+  );
